3326793ffde02031f86bdac9900e42ae8e8f2b7c
jssrinivas <113301344+jssrinivas@users.noreply.github.com>
2023-01-18 12:54:24 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -24,7 +24,7 @@ function myFuncDiv(){
   var num1=document.getElementById("num1").value;
   var num2=document.getElementById("num2").value;
   
-  documents.getElementById("para2").innerHTML = "The division of two numbers is: ";
+  document.getElementById("para2").innerHTML = "The division of two numbers is: ";
   
   if(num2===0){
     document.getElementById("para1").innerHTML = "Infinity";
@@ -38,7 +38,7 @@ function myFuncPow(){
   var num1=document.getElementById("num1").value;
   var num2=document.getElementById("num2").value;
   
-  documents.getElementById("para2").innerHTML = "The power of two numbers is: ";
+  document.getElementById("para2").innerHTML = "The power of two numbers is: ";
 
   document.getElementById("para1").innerHTML = (Math.pow(num1,num2));
   
